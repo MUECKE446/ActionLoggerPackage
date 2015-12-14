7f157c23fc3a3245958adfc5d9b6d6a7b2c49db0
Christian Muth <christian-muth@t-online.de>
2015-12-14 11:59:20 +0100

    first commit of ActionLogExample_1

---
+++ Examples/ActionLoggerExample_1/ActionLoggerExample_1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ActionLoggerExample_1
+//
+//  Created by Christian Muth on 13.12.15.
+//  Copyright © 2015 Christian Muth. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
